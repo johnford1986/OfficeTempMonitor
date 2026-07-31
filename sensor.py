f442468aa31546a593edb6793a58d61d042aa2c6
Ford <c68158@txt.textron.com>
2026-07-31 16:34:13 -0500

    Add debug logging to sensor

--- sensor.py
+++ sensor.py
@@ -1,20 +1,55 @@
 print("1 - sensor.py started")
 
-from dotenv import load_dotenv
+import os
+print("2 - os imported")
 
-print("2 - dotenv imported")
+from dotenv import load_dotenv
+print("3 - dotenv imported")
+
+print("4 - About to import tuya_connector")
 
 from tuya_connector import TuyaOpenAPI
 
-print("3 - tuya_connector imported")
+print("5 - Tuya connector imported")
 
-import os
-
-print("4 - os imported")
-
+print("6 - Loading .env")
 load_dotenv()
 
-print("5 - dotenv loaded")
+print("7 - .env loaded")
+
+ACCESS_ID = os.getenv("TUYA_ACCESS_ID")
+ACCESS_SECRET = os.getenv("TUYA_ACCESS_SECRET")
+ENDPOINT = os.getenv("TUYA_ENDPOINT")
+DEVICE_ID = os.getenv("TUYA_DEVICE_ID")
+
+
+def get_sensor_data():
+    print("8 - Creating API object")
+
+    openapi = TuyaOpenAPI(
+        ENDPOINT,
+        ACCESS_ID,
+        ACCESS_SECRET
+    )
+
+    print("9 - Connecting")
+
+    openapi.connect()
+
+    print("10 - Connected")
+
+    response = openapi.get(f"/v1.0/devices/{DEVICE_ID}")
+
+    print("11 - Got response")
+
+    status = response["result"]["status"]
+
+    data = {}
+
+    for item in status:
+        data[item["code"]] = item["value"]
+
+    return data
 
 ACCESS_ID = os.getenv("TUYA_ACCESS_ID")
 ACCESS_SECRET = os.getenv("TUYA_ACCESS_SECRET")
